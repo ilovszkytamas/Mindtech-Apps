8f4c9f67cb5654395e58241f162fa67277f137d4
tilovszky <v-tilovszky@expediagroup.com>
2024-03-03 19:31:59 +0100

    add swagger-ui

--- src/main/java/com/MindtechApps/MindtechApps/configuration/SecurityConfiguration.java
+++ src/main/java/com/MindtechApps/MindtechApps/configuration/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(request -> request.requestMatchers("/auth/**")
+            .authorizeHttpRequests(request -> request.requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**")
                 .permitAll().anyRequest().authenticated())
             .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
             .authenticationProvider(authenticationProvider()).addFilterBefore(
